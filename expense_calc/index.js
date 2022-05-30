0c4ce1990fba9878b09643fea28ef8320fe230a7
cordain <kordian.okularczyk@gmail.com>
2022-05-30 22:20:50 +0200

    Update site logic and style

--- expense_calc/index.js
+++ expense_calc/index.js
@@ -5,33 +5,68 @@ let wasm = await init();
 const expenses_list = document.getElementById("expenses_list");
 const add_expense = document.getElementById("add_expense");
 const calculate = document.getElementById("calculate");
+const buyer = document.getElementById("buyer");
+const amount = document.getElementById("amount");
+const owers = document.getElementById("owers");
 
 const ext_calculator = Calculator.new();
-
-expenses_list.textContent = ext_calculator.print_expenses();
+init_site()
 
 add_expense.addEventListener("click", event => {
-    const buyer = document.getElementById("buyer").value;
-    const amount = document.getElementById("amount").value;
-    const owers = document.getElementById("owers").value;
+    const buyer_name = buyer.value;
+    const amount_val = amount.value;
+    const owers_names = owers.value;
+    if(buyer_name == "" || amount_val == "" || owers_names == ""){
+        alert("Please fill the form");
+    }
+    else{
+        const idx = ext_calculator.add_expense(buyer_name,amount_val);
 
-    const idx = ext_calculator.add_expense(buyer,amount);
-
-    owers.split(new RegExp(", ?")).forEach(name => {
-        if (false == ext_calculator.add_ower_to_expense(name,idx)) {
-            alert("ERROR adding "+name);
-        }
-    });
-    expenses_list.textContent = ext_calculator.print_expenses();
+        owers.value.split(new RegExp(", ?")).forEach(name => {
+            if (false == ext_calculator.add_ower_to_expense(name,idx)) {
+                alert("ERROR adding "+name);
+            }
+        });
+        expenses_list.textContent = ext_calculator.print_expenses();
+    }
 })
 
 calculate.addEventListener("click", event => {
-    const report = document.getElementById("report");
-    report.innerHTML = "";
+    const table = document.getElementById("report");
+    const report = document.createElement("tbody");
+    table.innerHTML = "";
 
-    const tr = report.insertRow();
-    const td1 = tr.insertCell();
-    td1.appendChild(document.createTextNode(ext_calculator.calculate()));
+    const calc_report = ext_calculator.calculate();
 
-    report
+    
+    if(calc_report !== undefined){
+        const first_line_end = calc_report.indexOf('\n');
+        const trh = document.createElement("tr");
+        calc_report.substring(0,first_line_end).split(";").forEach(column_name => {
+            const th = document.createElement("th");
+            th.innerHTML = column_name;
+            trh.appendChild(th);
+        })
+        report.appendChild(trh);
+
+
+        const owe_report = calc_report.substring(first_line_end+1);
+        owe_report.split("\n").forEach(row => {
+            const tr = document.createElement("tr");
+            row.split(";").forEach(cell => {
+                const td = document.createElement("td");
+                td.innerHTML = cell;
+                tr.appendChild(td);
+            })
+            report.appendChild(tr);
+        })
+        table.appendChild(report);
+    }
 })
+
+function init_site(){
+    expenses_list.textContent = ext_calculator.print_expenses();
+    buyer.value = "";
+    amount.value = "";
+    owers.value = "";
+}
